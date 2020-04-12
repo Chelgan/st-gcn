ea0aef5cf0b85d60215301eb4ee50c67f8115fb7
Chelgan <48429665+Chelgan@users.noreply.github.com>
2020-04-12 15:49:15 +0800

    Update st_gcn.py

--- net/st_gcn.py
+++ net/st_gcn.py
@@ -36,11 +36,12 @@ class Model(nn.Module):
         self.register_buffer('A', A)
 
         # build networks
-        spatial_kernel_size = A.size(0)
+        spatial_kernel_size = A.size(0)         #should be a graph
         temporal_kernel_size = 9
         kernel_size = (temporal_kernel_size, spatial_kernel_size)
-        self.data_bn = nn.BatchNorm1d(in_channels * A.size(1))
-        kwargs0 = {k: v for k, v in kwargs.items() if k != 'dropout'}
+        self.data_bn = nn.BatchNorm1d(in_channels * A.size(1))          #↓refer https://www.cnblogs.com/mgblog/p/11459628.html
+        kwargs0 = {k: v for k, v in kwargs.items() if k != 'dropout'}   #update parameters by kwargs(a dict)
+        #netframework
         self.st_gcn_networks = nn.ModuleList((
             st_gcn(in_channels, 64, kernel_size, 1, residual=False, **kwargs0),
             st_gcn(64, 64, kernel_size, 1, **kwargs),
